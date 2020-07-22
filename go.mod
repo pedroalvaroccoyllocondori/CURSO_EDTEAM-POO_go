67dd0c4bb8b709a47ada5bba644df5179ad35051
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-22 10:57:57 -0500

    creacion de modulo en go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pedroalvaroccoyllocondori/POO_go/cursos
+
+go 1.14
--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
-
 import "fmt"
 
-
-
-
-func main(){
+func main() {
 	//instancia especificando los atributos
-	 Go := curso{
-		 nombre:"javascrip desde cero",
-		 precio:68.05,
-		 esgratis:false,
-		 idusuarios:[]uint{01,02,03},
-		 clases: map[uint]string{
-			 1:"introducion",
-			 2:"estructuras",
-			 3:"mapas",
-		 },	 
-	 }
-	 (&Go).cambiarprecio(100.66)
-	 fmt.Println(Go.precio)
-
+	Go := curso{
+		nombre:     "javascrip desde cero",
+		precio:     68.05,
+		esgratis:   false,
+		idusuarios: []uint{01, 02, 03},
+		clases: map[uint]string{
+			1: "introducion",
+			2: "estructuras",
+			3: "mapas",
+		},
+	}
+	(&Go).cambiarprecio(100.66)
+	fmt.Println(Go.precio)
 
 }
