aa040199745cc75078bd224e7ecf903b67904906
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-23 16:48:28 -0500

    composicion de estructuras finalizado

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pedroalvaroccoyllocondori/POO_go/pkg/cliente"
+	"github.com/pedroalvaroccoyllocondori/POO_go/pkg/factura"
+	"github.com/pedroalvaroccoyllocondori/POO_go/pkg/idfactura"
+)
+
 func main() {
 
+	objeto := factura.Nuevo(
+		"peru",
+		"ica",
+		cliente.Nuevo("alvaro", "ica", "917186613"),
+		[]idfactura.Item{
+			idfactura.Nuevo(01, "curso de git", 12.3),
+			idfactura.Nuevo(02, "curso de android", 12.3),
+			idfactura.Nuevo(03, "curso de go", 67.3),
+		},
+	)
+	objeto.Establecertotal()
+	fmt.Printf("%+v", objeto)
 }
