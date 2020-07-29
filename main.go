850e523d7f6ced3ee99c72c66512664ca9e21780
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-28 23:08:21 -0500

    type asertion( cuando no sabemos que tipo de interfaz se va recibir en  un metodo)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type ejemplo interface {
@@ -10,6 +11,12 @@ type ejemplo interface {
 
 func envoltura(interfaz interface{}) { //metodo que recibe una interfaz vacia
 	fmt.Printf("valor :%v tipo:%T \n", interfaz, interfaz)
+	//validar el tipo de  interfaz que ingresa al metodo
+
+	valor, ok := interfaz.(string) //guardando  y validando el valor y el error que producen
+	if ok {
+		fmt.Printf("\t%s\n", strings.ToUpper(valor))
+	}
 }
 
 func main() {
@@ -17,6 +24,7 @@ func main() {
 	//var ejemplar ejemplo // declarando una interfaz nula (ninguna clase a implementado ese metodo)
 	//ejemplar.metodo1()
 	envoltura(12)
+	envoltura("matusculas")
 	envoltura(12.23)
 	envoltura("cadena")
 	envoltura(true)
