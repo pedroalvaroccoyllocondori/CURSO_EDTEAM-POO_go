5d9f8d2c57946b008ec83ef593b6f7b474926cb0
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-28 22:56:16 -0500

    interfaces vacias(cuando no sabemos el tipo que se va a recibir)

--- main.go
+++ main.go
@@ -4,55 +4,21 @@ import (
 	"fmt"
 )
 
-type MetodoPago interface {
-	Pagar()
+type ejemplo interface {
+	metodo1()
 }
 
-type Paypal struct{}
-
-func (referencia Paypal) Pagar() { // implemtacion de la interfaz
-	fmt.Println("pagado por paypal")
-}
-
-type Tarjeta struct{}
-
-func (referencia Tarjeta) Pagar() { // implemtacion de la interfaz
-	fmt.Println("pagado  con tarjeta")
-}
-
-type Efectivo struct{}
-
-func (referencia Efectivo) Pagar() { // implemtacion de la interfaz
-	fmt.Println("pagado por  efectivo")
-}
-
-func Cajero(tipopago uint) MetodoPago { // metodo que devuelve una interfaz
-	switch tipopago {
-	case 1:
-		return Paypal{}
-	case 2:
-		return Tarjeta{}
-	case 3:
-		return Efectivo{}
-	default:
-		return nil
-
-	}
+func envoltura(interfaz interface{}) { //metodo que recibe una interfaz vacia
+	fmt.Printf("valor :%v tipo:%T \n", interfaz, interfaz)
 }
 
 func main() {
 
-	var metodo uint // declaracion de la variable
-	fmt.Println("digite el metodo de pago \n 1: paypal \n 2: tarjeta \n 3: efectivo")
-	_, error := fmt.Scanln(&metodo)
-	if error != nil { // si se inserta letra
-		panic("debes diguitar un numero entre 1 y 3 y no letras")
-	}
-	if metodo > 3 { // si se insert numeros mayores a 3
-		panic("debes diguitar un numero entre 1 y 3 y no mayores a ese rango")
-	}
-
-	metodopago := Cajero(metodo) //el metodo devuelve una interfaz
-	metodopago.Pagar()
+	//var ejemplar ejemplo // declarando una interfaz nula (ninguna clase a implementado ese metodo)
+	//ejemplar.metodo1()
+	envoltura(12)
+	envoltura(12.23)
+	envoltura("cadena")
+	envoltura(true)
 
 }
