1be299deafef5425872879ca29da6000816537e9
alvaro <pedro_alvaro@outlook.com.pe>
2020-08-04 11:17:02 -0500

    funciones decoradoras 1ra parte

---
+++ funciones/funciones.go
@@ -0,0 +1,11 @@
+package funciones
+
+import "fmt"
+
+func Saludar(nombre string) {
+	fmt.Println("hola", nombre)
+}
+
+func Despedirse(nombre string) {
+	fmt.Println("adios", nombre)
+}
--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "github.com/pedroalvaroccoyllocondori/POO_go/funciones"
 
-type ejemplo interface {
-	metodo1()
+func ejecutar(nombre string, funcion func(string)) {
+	funcion(nombre)
 }
 
-func envoltura(interfaz interface{}) { //metodo que recibe una interfaz vacia
-
-	switch valor := interfaz.(type) {
-	case string:
-		fmt.Printf("\t%s\n", strings.ToUpper(valor))
-	case int:
-		fmt.Println(valor * 5)
-	default:
-		fmt.Printf("valor :%v tipo:%T \n", interfaz, interfaz)
-		//validar el tipo de  interfaz que ingresa al metodo
-
-	}
-}
 func main() {
 
-	//var ejemplar ejemplo // declarando una interfaz nula (ninguna clase a implementado ese metodo)
-	//ejemplar.metodo1()
-	envoltura(12)
-	envoltura("matusculas")
-	envoltura(12.23)
-	envoltura("cadena")
-	envoltura(true)
-
+	nombre := "golan en español"
+	ejecutar(nombre, funciones.Saludar)
+	ejecutar(nombre, funciones.Despedirse)
 }
