b000e7d04a0df138eb9c165598fd7979fb7ed28d
alvaro <pedro_alvaro@outlook.com.pe>
2020-08-04 11:24:47 -0500

    funciones decoradoras parte 2

--- funciones/funciones.go
+++ funciones/funciones.go
@@ -1,11 +1,16 @@
 package funciones
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Saludar(nombre string) {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println("hola", nombre)
 }
 
 func Despedirse(nombre string) {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println("adios", nombre)
 }
